54eda852682aaca6da728e35a67cbbb06a98f814
mariavelma <mariavelma1@gmaol.com>
2022-06-15 00:20:31 +0700

    velma tugas

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,19 @@
+<!-- Menghubungkan dengan view template master -->
+@extends('layout/template')
+
+@section('title','TENTANG')
+<!-- isi bagian judul halaman -->
+<!-- cara penulisan isi section yang pendek -->
+@section('judul', 'Halaman Tentang')
+
+<!-- isi bagian konten -->
+<!-- cara penulisan isi section yang panjang -->
+@section('konten')
+
+  <p>Ini Adalah Halaman Tentang</p>
+  <p>
+    Maria Velma De Jaseda Maria Velma De Jaseda Maria Velma De Jaseda Maria Velma De Jaseda Maria Velma De Jaseda Maria Velma De Jaseda
+    Maria Velma De Jaseda Maria Velma De Jaseda Maria Velma De Jaseda Maria Velma De Jaseda Maria Velma De Jaseda Maria Velma De Jaseda
+  </p>
+
+@endsection
